2922d9c34691ff2ab2b5d9097df14953719594f7
indiansage <indiansage.work@gmail.com>
2020-08-01 22:08:03 +0530

    basic structure

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+const router = require('./routes');
+const db = require('./config/mongoose');
+const tasks = require('./models/task');
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use('/', router);
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log('error');
+    }
+});
+
+// app.get('/', (req,res) => {
+//     return res.render('home');
+// })
